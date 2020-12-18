716f96d30474e92f4feb52611c088fd8e911a8f8
Arthur Santana <artsantana1457@gmail.com>
2020-12-18 19:32:25 -0300

    Created theme for application

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    nightPurple: '#5741D7',
+    sunnyOrange: '#FD9B3D',
+    rainyBlue: '#4A79F3',
+    gray: '#72788A',
+    textDark: '#343434',
+    error: '#FF453A',
+  },
+};
+
+export default theme;
