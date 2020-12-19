095a2dec34ee8c8fb3e7e6c4b8c796cb6d25c969
Arthur Santana <artsantana1457@gmail.com>
2020-12-18 23:52:35 -0300

    Login screen structure

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,77 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {Keyboard} from 'react-native';
+import {Button, Input} from '../../components';
+import theme from '../../styles/theme';
+import {
+  Container,
+  DismissKeyboard,
+  Footer,
+  ForgotPassword,
+  ForgotPasswordLabel,
+  Message,
+} from './styles';
 
 const Login: React.FC = () => {
-  return <></>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const refPassword = useRef();
+
+  const onChangeEmail = (text: string) => {
+    setEmail(text);
+  };
+
+  const onChangePassword = (text: string) => {
+    setPassword(text);
+  };
+
+  const onPressButton = () => {};
+
+  const onSubmitEmail = () => {
+    refPassword.current.focus();
+  };
+
+  return (
+    <DismissKeyboard onPress={Keyboard.dismiss}>
+      <Container>
+        <Input
+          autoCapitalize="none"
+          autoCompleteType="email"
+          keyboardType="email-address"
+          materialIcon="email"
+          onChangeText={onChangeEmail}
+          onSubmitEditing={onSubmitEmail}
+          placeholder="E-Mail"
+          placeholderTextColor={theme.colors.placeholder}
+          returnKeyType="next"
+          textContentType="emailAddress"
+          value={email}
+        />
+        <Input
+          onChangeText={onChangePassword}
+          placeholder="Password"
+          placeholderTextColor={theme.colors.placeholder}
+          materialIcon="vpn-key"
+          onSubmitEditing={onPressButton}
+          refProp={refPassword}
+          returnKeyType="done"
+          secureTextEntry={true}
+          textContentType="password"
+          value={password}
+        />
+        <ForgotPassword>
+          <ForgotPasswordLabel>Forgot Password?</ForgotPasswordLabel>
+        </ForgotPassword>
+        <Button label="Sign In" onPress={onPressButton} />
+
+        <Footer>
+          <Message>
+            Don't have an account yet?
+            <ForgotPasswordLabel> Sign Up</ForgotPasswordLabel>
+          </Message>
+        </Footer>
+      </Container>
+    </DismissKeyboard>
+  );
 };
 
 export {Login};
