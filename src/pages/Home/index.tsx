203270a7a68a6d2a978f2f4d89311c7f7b28c860
Arthur Santana <artsantana1457@gmail.com>
2020-12-25 21:23:24 -0300

    Added trigger to open and close drawer on home screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import theme from '../../styles/theme';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {DrawerActions, useNavigation} from '@react-navigation/native';
+import {StatusBar} from 'react-native';
+import {Container, DrawerToggle} from './styles';
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 const Home: React.FC = () => {
-  return <></>;
+  const navigation = useNavigation();
+
+  const onPressDrawer = () => {
+    navigation.dispatch(DrawerActions.toggleDrawer());
+  };
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.nightPurple}
+      />
+      <DrawerToggle onPress={onPressDrawer}>
+        <MaterialIcons color="#fff" name="menu" size={hp(4.5)} />
+      </DrawerToggle>
+    </Container>
+  );
 };
 
 export {Home};
