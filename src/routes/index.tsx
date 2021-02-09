72c1f347044446961b5ea7a8213e56235c8446e5
Arthur Santana <artsantana1457@gmail.com>
2021-02-08 23:28:12 -0300

    chore: created provider in app and consumes in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {InsideApp} from './InsideApp';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthStack} from './AuthStack';
+import {UserContext} from '@models/UserContext';
 
 function Routes() {
+  const {user} = useContext(UserContext);
+
   return (
     <NavigationContainer>
-      {false ? <AuthStack /> : <InsideApp />}
+      {user.isSigned ? <AuthStack /> : <InsideApp />}
     </NavigationContainer>
   );
 }
